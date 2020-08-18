ac6f441ab65bbcb77850dbb736c519d93b6ffbbd
beeguy74 <beeguy74@gmail.com>
2020-08-18 13:38:45 +0500

    very very memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+void	*ft_memcpy(void* dest, const void *src, size_t len)
+{
+	unsigned char	*tmp;
+	unsigned char	*res;
+
+	tmp = (unsigned char*)src;
+	res = (unsigned char*)dest;
+	if (len == 0 || src == dest)
+	{
+		return (dest);
+	}
+	while(len--)
+	{
+		res[len] = tmp[len];
+	}
+	return (dest);
+}
