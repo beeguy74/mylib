724379b184dd0cf3cf272306c002f3405846fd4a
beeguy74 <beeguy74@gmail.com>
2020-08-19 16:40:48 +0500

    head with memccpy

--- head.h
+++ head.h
@@ -6,5 +6,6 @@
 void	*ft_bzero(void* b, size_t len);
 void	*ft_memcpy(void* dest, const void *src, size_t len);
 void	*ft_memset(void* b, int c, size_t len);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 
 #endif
