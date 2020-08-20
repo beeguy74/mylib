28021012ab16c591e35128fea08313fff5784bf3
beeguy74 <beeguy74@gmail.com>
2020-08-20 16:40:16 +0500

    something went wrong with move

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+#include "head.h" 
+
+void	*ft_memmove(void* dest, const void *src, size_t len)
+{
+	unsigned char	*srt;
+	unsigned char	*tmp;
+	unsigned char	*res;
+	size_t		i;
+
+	i = len;
+	srt = (unsigned char*)src;
+	res = (unsigned char*)dest;
+	if (len == 0 || src == dest)
+	{
+		return (dest);
+	}
+	tmp = (unsigned char*)malloc(sizeof(char) * len);
+	if (!srt)
+		return (NULL);
+	while(i--)
+	{
+		tmp[i] = srt[len];
+	}
+	while(len--)
+	{
+		res[len] = tmp[len];
+	}
+	free(tmp);
+	return (dest);
+}
