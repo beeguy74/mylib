eb57241bbf4f0440602c8912b59a96a1cad1e5e9
beeguy74 <beeguy74@gmail.com>
2020-08-21 13:18:09 +0500

    cmp

---
+++ ft_memcmp.c
@@ -0,0 +1,15 @@
+#include "head.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	while (n--)
+	{
+		if (*(unsigned char*)s1 < *(unsigned char*)s2)
+			return (-1);
+		else if (*(unsigned char*)s1 > *(unsigned char*)s2)
+			return (1);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
